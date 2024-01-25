0add42d8b14febfc83ce48a0475f272fc2e94892
siro-neko1101 <kuroneko3115@gmail.com>
2024-01-25 16:39:49 +0900

    static_url 追加

--- Household_account/Household_account/settings.py
+++ Household_account/Household_account/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_URL = 'static/'
 
 AUTH_USER_MODEL = 'accounts.User'
 ROGIN_URL = '/accounts/login'
